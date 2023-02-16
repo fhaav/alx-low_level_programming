94340d6cde41e9ed67361b39d244d32ec3669449
Favour <mzfavvy20@gmail.com>
2023-02-17 04:04:35 +0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
-init main(void)
+int main(void)
 {
-	puts(*\*Programming is like building a multilingual puzzle*)
-		return (0);
+	puts(*\*Programming is like building a multilingual puzzle*);
+	
+
+	return (0);
 }	
