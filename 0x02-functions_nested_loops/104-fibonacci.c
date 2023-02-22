0cf6ddfa094ec749ac3af951c991b04f4fc279ea
Favour <mzfavvy20@gmail.com>
2023-02-23 04:58:24 +0800

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - main block
- *
- * Description: finds and prints the first 98 Fibonacci numbers,
+ * main - Prints the first 98 Fibonacci numbers,
  * starting with 1 and 2, followed by a new line.
  *
  * Return: Always 0
@@ -34,7 +32,7 @@ int main(void)
 	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
 	half1 += 1;
-	half2 %= 100000000000;
+	half2 %= 10000000000;
 	}
 	printf("%lu%lu", half1, half2);
 	if (count != 98)
