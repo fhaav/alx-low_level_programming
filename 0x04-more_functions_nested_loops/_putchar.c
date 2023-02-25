36572897495f04cbedc2ca72c81a1fc23951254c
Favour <mzfavvy20@gmail.com>
2023-02-26 07:05:20 +0800

    0-isuper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return 1 if true, 0 if false
+ * On error, return -1.
+ */
+int_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
