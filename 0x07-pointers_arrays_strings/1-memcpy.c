9e8b84c4886c0ee3a6374e5d1b2e445815669cf4
Favour <mzfavvy20@gmail.com>
2023-03-07 05:57:19 +0800

    Mandatory Tasks

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ *
+ * @n: number of bytes
+ * @src: memory where is copied
+ * @dest: memory where is stored
+ *
+ * Return: copied memory with n byte changed
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
