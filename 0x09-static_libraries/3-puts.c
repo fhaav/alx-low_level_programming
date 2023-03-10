6571e70c3ac7acceae54a079082784eb9a3cdf81
Favour <mzfavvy20@gmail.com>
2023-03-11 02:59:12 +0800

    liball

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,10 @@
 #include "main.h"
-#include<stdio.h>
-
-/**
- * _puts - Entry point
- *
- * @s: input value
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts(s);
 }
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc && ar -rc liball.a *.o -c *.c
+gcc -Wall -Wextra -Werror -pedantic -c *.c
+ar rc liball.a *.o
