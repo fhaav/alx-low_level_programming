fac52cad257e66a4bace0b4b9665c3f84f161b43
Favour <mzfavvy20@gmail.com>
2023-03-23 17:34:48 +0800

    task 3 update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - prints result
+ * main - prints the result of simple operations.
  *
  * @argc: Number of arguments
  * @argv: Array of pointers to the arguments
@@ -13,7 +13,9 @@
 
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,14 +23,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
-	if (!oprt)
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
 	return (0);
 }
