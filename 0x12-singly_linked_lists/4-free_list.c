f1179dc41d854463a437be5e56ad911966235c11
Favour <mzfavvy20@gmail.com>
2023-03-30 17:43:59 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a list_t list.
+ *
+ * @head: pointer to list_t list.
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
