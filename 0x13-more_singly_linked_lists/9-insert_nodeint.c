13f7031b7f7137379142b59f674a043158de6c4d
Favour <mzfavvy20@gmail.com>
2023-04-05 04:17:15 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,18 +16,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newnode;
 	unsigned int pos;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
-	if (idx != 0)
-	{
-		temp = *head;
-		for (pos = 0; pos < idx - 1 && temp != NULL; pos++)
-		{
-			temp = temp->next;
-		}
-		if (temp == NULL)
-			return (NULL);
-	}
+
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
@@ -38,6 +29,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newnode->next = *head;
 		*head = newnode;
 		return (newnode);
+	}
+
+	else if (idx != 0)
+	{
+		temp = *head;
+		for (pos = 0; pos < idx - 1 && temp != NULL; pos++)
+		{
+			temp = temp->next;
+		}
+		if (temp == NULL)
+			return (NULL);
 	}
 	newnode->next = temp->next;
 	temp->next = newnode;
