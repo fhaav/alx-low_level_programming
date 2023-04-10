571cffc89719cea167b0318ee2cd89cf610a58ef
Favour <mzfavvy20@gmail.com>
2023-04-10 03:10:02 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,11 +21,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL && text_content[len] != '\0')
-		len++;
+	if (text_content != NULL && text_content[len])
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 	nw = write(fd, text_content, len);
 
-	if (nw == -1 || nw != len)
+	if (nw == -1 || fd == -1)
 	{
 		close(fd);
 		return (-1);
