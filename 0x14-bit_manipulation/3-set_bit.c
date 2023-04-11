3f2e5f811331913a0f9ade5c4736bc28805c6268
Favour <mzfavvy20@gmail.com>
2023-04-11 13:42:10 -0700

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * count_bits - ...
- * @n: ...
- * Return: ...
- */
-
-unsigned int count_bits(unsigned long int n)
-{
-	unsigned int count = 0;
-
-	while (n != 0)
-	{
-		count += n & 1;
-		n >>= 1;
-	}
-	return (count);
-}
-
-/**
  * set_bit - a function that sets the value of a bit to 1 at a given index.
  * @n: pointer
  * @index: the index, starting from 0 of the bit you want to set.
@@ -27,16 +9,9 @@ unsigned int count_bits(unsigned long int n)
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int num_bits;
-
-	if (index > 63)
+	if (index >= sizeof(unsigned long int) * CHAR_BIT)
 		return (-1);
 
-	num_bits = count_bits(*n);
-	if (index >= num_bits)
-	{
-		*n |= 1UL << index;
-		return (0);
-	}
-	return (-1);
+	*n |= 1UL << index;
+	return (0);
 }
