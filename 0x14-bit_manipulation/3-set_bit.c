d069b4149ddf8c2b6366d37d692a4edabbb159cd
Favour <mzfavvy20@gmail.com>
2023-04-11 12:58:50 -0700

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -29,7 +29,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int num_bits;
 
-	if (index >= sizeof(unsigned long int) * CHAR_BIT)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
