74dd88fb944ab8b125301e022056546e118d553a
Favour <mzfavvy20@gmail.com>
2023-04-11 12:35:45 -0700

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * count_bits - ...
+ * @n: ...
+ * Return: ...
+ */
+
+unsigned int count_bits(unsigned long int n)
+{
+	unsigned int count = 0;
+
+	while (n != 0)
+	{
+		count += n & 1;
+		n >>= 1;
+	}
+	return (count);
+}
+
+/**
+ * set_bit - a function that sets the value of a bit to 1 at a given index.
+ * @n: pointer
+ * @index: the index, starting from 0 of the bit you want to set.
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int num_bits;
+
+	if (index >= sizeof(unsigned long int) * CHAR_BIT)
+	{
+		return (-1);
+	}
+
+	num_bits = count_bits(*n);
+	if (index >= num_bits)
+	{
+		return (-1);
+	}
+
+	*n |= (1UL << index);
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <limits.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -10,5 +11,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+unsigned int count_bits(unsigned long int n);
 
 #endif
