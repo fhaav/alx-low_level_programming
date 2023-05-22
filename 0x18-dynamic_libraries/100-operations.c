6569b09f7dc8cf2cb74ca9ae50cf426414c9ea2d
Favour <mzfavvy20@gmail.com>
2023-05-22 07:05:45 -0700

    implemented a dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+
+/**
+ * add - Function that adds two integers.
+ * @a: Integer operand.
+ * @b: Integer operand.
+ * Return: The sum of the two integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Function that subtracts two integers.
+ * @a: Integer operand.
+ * @b: Integer operand.
+ * Return: The difference between the two integers.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Function that multiplies two integers.
+ * @a: Integer operand.
+ * @b: Integer operand.
+ * Return: The product of the two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Function that calculates the quotient of two integers.
+ * @a: Integer dividend.
+ * @b: Integer divisor.
+ * Return: The quotient of the two integers.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Function that calculates the remainder of two integers.
+ * @a: Integer dividend.
+ * @b: Integer divisor.
+ * Return: The remainder of the two integers.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
